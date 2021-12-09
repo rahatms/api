f770a57ac337c545842e2d7d490a6e0efef282e0
Rahat Muneeb <rahat@pop-os.localdomain>
2021-12-09 04:04:28 +0000

    init

---
+++ src/Server.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const server = express();
+const utils = require("./utils");
+
+server.use(express.json());
---
+++ src/utils.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose");
+
+const BookSchema = new mongoose.Schema({
+    title: { type: String, index: true, unique: true },
+    author: String,
+    published: String,
+    edition: String,
+    publisher: String,
+})
+
+const Book = mongoose.model("Book", BookSchema);
+
+const connect =  () => {
+    return mongoose.connect("mongodb://localhost:27017/api");
+};
+
+const connectAndStartServer = (server, port) => {
+    return mongoose.connect("mongodb://localhost:27017/api").then(() => {
+        console.log("Connected to database!")
+        server.listen(port, () => {
+            console.log(`Server listening on port:${port}`);
+        })
+    }).catch((err) => {
+        console.log("Error Happened while starting server!\n", err);
+    })
+};
+
+const getBooks = async (options = {}) => {
+    const books = await Book.find(options);
+    return books;
+};
+
+const createBook = async (book) => {
+    const newBook = new Book(book);
+    await newBook.save();
+    return newBook;
+};
+
+const deleteBook = async (options = {}) => {
+    if (Object.keys(options).length == 0) {
+        throw Error("No delete parameter provided!");
+    }
+    return await Book.deleteOne(options);
+};
+
+const updateBook = async (filter, values) => {
+    const book = await Book.updateOne(filter, values);
+    return book;
+
+};
+
+module.exports = {
+    connect,
+    connectAndStartServer,
+    Book,
+    getBooks,
+    createBook,
+    deleteBook,
+    updateBook,
+};
